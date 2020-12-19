d5af5649b9c15a76163a1dea124649a1ddc08295
Alyson Maruyama <alyson1907@gmail.com>
2020-12-18 22:16:14 -0300

    Renames App --> Home and removes unused code

---
+++ src/containers/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles/Home.scss'
+
+const Home:React.FC = () => {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+      </header>
+    </div>
+  )
+}
+
+export default Home
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.scss'
-import App from './App'
+import App from './containers/Home'
 import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
