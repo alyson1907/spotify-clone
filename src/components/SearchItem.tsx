3f5bec380f2c153a1300a5bf52f78c2008e88c24
Alyson Maruyama <alyson1907@gmail.com>
2020-12-29 17:40:24 -0300

    Implemented SearchItem and items on Search page

---
+++ src/components/SearchItem.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import 'components/styles/SearchItem.scss'
+
+export type Props = {
+  title: string
+  img: string
+  backgroundColor: string
+  onClick?: () => void
+}
+
+const handle3DigitsHex = (hex: string) => {
+  if (hex.length === 4) {
+    return (
+      '#' +
+      hex
+        .split('#')
+        .pop()
+        ?.split('')
+        .reduce((acc, ch) => (acc += `${ch}${ch}`), '')
+    )
+  }
+  return hex
+}
+
+const SearchItem: React.FC<Props> = ({ title, img, backgroundColor, onClick }) => {
+  backgroundColor = handle3DigitsHex(backgroundColor)
+  return (
+    <div
+      className='search-item-container'
+      style={{ background: `linear-gradient(0deg,${backgroundColor},${backgroundColor}77)` }}
+      onClick={onClick}>
+      <h3>{title}</h3>
+      <img src={img} className='search-item-image' />
+    </div>
+  )
+}
+
+SearchItem.defaultProps = {
+  title: '',
+  img: 'https://picsum.photos/200',
+  backgroundColor: '#ccc',
+  onClick: () => {
+    console.log('SearchItem clicked')
+  },
+}
+
+export default SearchItem
--- src/containers/Search.tsx
+++ src/containers/Search.tsx
@@ -1,8 +1,56 @@
 import React from 'react'
+import SearchItem, { Props as SearchItemProps } from 'components/SearchItem'
 import 'containers/styles/Search.scss'
 
+const items = [
+  {
+    title: 'Podcasts',
+    img: `https://picsum.photos/200?t=${Math.random()}`,
+    backgroundColor: '#f93',
+    onClick: () => {},
+  },
+  {
+    title: 'Made For You',
+    img: `https://picsum.photos/200?t=${Math.random()}`,
+    backgroundColor: '#bcf',
+    onClick: () => {},
+  },
+  {
+    title: 'New Releases',
+    img: `https://picsum.photos/200?t=${Math.random()}`,
+    backgroundColor: '#ddf',
+    onClick: () => {},
+  },
+  {
+    title: 'Discover',
+    img: `https://picsum.photos/200?t=${Math.random()}`,
+    backgroundColor: '#c9f',
+    onClick: () => {},
+  },
+]
+
+const renderSearchItems: React.FC<Array<SearchItemProps>> = (searchItems) => {
+  return (
+    <>
+      {searchItems.map((item) => (
+        <SearchItem title={item.title} img={item.img} backgroundColor={item.backgroundColor} />
+      ))}
+    </>
+  )
+}
+
 const Search: React.FC = () => {
-  return <h2 style={{ color: 'green' }}>Search Page Here!</h2>
+  return (
+    <div className='search-container'>
+      <h2>Browse all</h2>
+      <div className='search-items'>
+        {renderSearchItems(items)}
+        {renderSearchItems(items)}
+        {renderSearchItems(items)}
+        {renderSearchItems(items)}
+      </div>
+    </div>
+  )
 }
 
 export default Search
